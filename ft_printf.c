ce79f95d8641428edcde9b846876c9f6c296f5be
hfilipe- <hfilipe-@student.42porto.com>
2024-11-19 19:10:32 +0000

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hfilipe- <hfilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 15:17:25 by hfilipe-          #+#    #+#             */
-/*   Updated: 2024/11/19 18:03:46 by hfilipe-         ###   ########.fr       */
+/*   Updated: 2024/11/19 19:04:51 by hfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,67 +14,63 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int ft_printf(const char *format, ...)
+int ft_printf(const char *str, ...)
 {
 	va_list args;
-    va_start(args, format);
-	char	c;
-	char	*str;
-	int		d;
-	int 	x;
+    va_start(args, str);
+	size_t	i;
 
-	while (*format)
+	i = 0;
+	while(str)
 	{
+		if (*str == '%')
+		{
+			while (*str != '%' || *str != '\0')
+			{
+				i++;
+				str++;
+			}
+			select_formats(args - i, str);
+		}
+		str++;
+	}
+
+	va_end(args);
+}
+
+int select_formats(va_list args, const char *format)
+{
+
         if (*format == '%')
 		{
             format++;
+			if (*format == '0')
+				//fill the empty spaces with 0. need to do string lenght of the rest of the output and thens put the rest with 0
+			if (*format == '+')
+				//call function putnbr and show + if its positiv or - if negative  (%d, %i)
+			if (*format == ' ')
+				//call function epspaco put space before output (alinha os prints) adiciona um space adicioanl se for positivo
+			if (*format == '#')
+				//call function to add 0x if x or 0X if X before the rest of number 
 			if (*format == 'c')
-			{
-				c = va_arg(args, char);
-				ft_putchar_fd(c, 0);
-			}
+				ft_putchar_fd(va_arg(args, char), 0);
 			else if (*format == 's')
-			{
-				str = va_arg(args, char *);
-				ft_putstr_fd(str, 0);
-			}
+				ft_putstr_fd(va_arg(args, char *), 0);
 			else if (*format == 'p')
-			{
-				x = va_arg(args, int);
-				ft_puttnbr_hexa_l(x, 0);
-			}
+				ft_puttnbr_hexa_l(va_arg(args, int), 0);
 			else if (*format == 'd')
-			{
-				x = va_arg(args, int);
-				ft_putnbr_fd(d, 0);
-			}
+				ft_putnbr_fd(va_arg(args, int), 0);
 			else if (*format == 'i')
-			{
-				x = va_arg(args, int);
-				ft_putnbr_fd(d, 0);
-			}
+				ft_putnbr_fd(va_arg(args, int), 0);
+								 // needs to round the number 
 			else if (*format == 'u')
-			{
-				x = va_arg(args, unsigned int);
-				ft_putnbr_fd_ui(d, 0);
-			}
+				ft_putnbr_fd_ui(va_arg(args, unsigned int), 0);
 			else if (*format == 'x')
-			{
-			x = va_arg(args, int);
-				ft_puttnbr_hexa_l(x, 0);
-			}
+				ft_puttnbr_hexa_l(va_arg(args, int), 0);
 			else if (*format == 'X')
-			{
-				x = va_arg(args, int);
-				ft_puttnbr_hexa_u(x, 0);
-			}
+				ft_puttnbr_hexa_u(va_arg(args, int), 0);
 			else if (*format == '%')
-			{
-				c = va_arg(args, char);
 				ft_putchar_fd('%', 0);
-			}
 		}
 		format++;
 	}
-	va_end(args);
-}
